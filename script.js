09076e315d6828df47984e23ae1528d2a3a0ec12
bhaskar0509 <bhaskar050903@gmail.com>
2024-10-13 11:22:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./js/service-worker.js').then(() => {
-        console.log('Service Worker registered');
-    }).catch((error) => {
-        console.error('Service Worker registration failed:', error);
-    });
-}
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('service-worker.js') // Ensure the path is correct
+                    .then((registration) => {
+                        console.log('Service Worker registered with scope:', registration.scope);
+                    })
+                    .catch((error) => {
+                        console.error('Service Worker registration failed:', error);
+                    });
+            });
+        }
 
 
 
