8963d495386e6e09122aed65eda67c3777ed82c0
bhaskar0509 <bhaskar050903@gmail.com>
2024-10-13 21:24:50 +0530

    add files

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/jarvis2.0/service-worker.js').then(() => {
-        console.log('Service Worker registered');
-    }).catch((error) => {
-        console.error('Service Worker registration failed:', error);
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/jarvis2.0/sw.js').then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch((error) => {
+            console.error('Service Worker registration failed:', error);
+        });
     });
 }
+
+  
 
 
 
